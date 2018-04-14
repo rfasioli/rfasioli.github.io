5064e521560e7d02497df013367422be22ac2122
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2018-04-14 09:22:35 -0700

    animation fixes, translate, cv improvements

---
+++ assets/js/dict.js
@@ -0,0 +1,18 @@
+class Dicionario {
+  static returnT() {
+    return {
+      "lbl_ptbr": {
+        pt: "Português",
+        en: "Portuguese"
+      },
+      "lbl_en":{
+        pt: "Inglês",
+        en: "English"
+      },
+      "text_job":{
+        pt :"Analista <span>Desenvolvedor</span>",
+        en :"Analyst <span>developer</span>"
+      }
+    }
+  }
+}
